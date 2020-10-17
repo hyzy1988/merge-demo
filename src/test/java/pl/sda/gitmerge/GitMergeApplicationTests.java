63e023217398a8ce96d20b5b0c38fe82342bc3a5
maniek <mpastuszka@gmail.com>
2020-10-17 11:38:43 +0200

    [init] - initial commit

---
+++ src/test/java/pl/sda/gitmerge/GitMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.gitmerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMergeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
