96a35fe126acbe246c716d35bbbfd71919f24ade
Tomek <tomekpl77@gmail.com>
2020-10-17 12:40:15 +0200

    [tomek] - first commit - Person service creation

---
+++ src/main/java/pl/sda/gitmerge/service/PersonService.java
@@ -0,0 +1,18 @@
+package pl.sda.gitmerge.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import pl.sda.gitmerge.domain.Person;
+
+@Service
+@Slf4j
+public class PersonService {
+
+    public Person getPerson() {
+        return Person.builder()
+                .name("Tomek")
+                .surname("Kowalski")
+                .address("Ul. Krzywa 13")
+                .build();
+    }
+}
