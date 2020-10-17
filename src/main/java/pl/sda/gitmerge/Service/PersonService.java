244090cd9a7c507a93200845dddada8f87b60197
PiotrCzechowski <czechowp@gmail.com>
2020-10-17 14:46:59 +0200

    [add] Rest controler 2

--- src/main/java/pl/sda/gitmerge/Service/PersonService.java
+++ src/main/java/pl/sda/gitmerge/Service/PersonService.java
@@ -1,8 +1,5 @@
 package pl.sda.gitmerge.Service;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.sda.gitmerge.domain.Person;
 
